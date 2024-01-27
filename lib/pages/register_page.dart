6145d9e37e922bba04c53cf7d15bebf4fb825dc5
Khip01 <aakhifathallah24@gmail.com>
2024-01-27 22:18:06 +0700

    feat: added register page style only

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,10 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
 
   @override
+  State<RegisterPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<RegisterPage> {
+  final GlobalKey _formKey = GlobalKey();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: SizedBox(
+            height: 500,
+            width: 400,
+            child: Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(3)),
+              elevation: 4,
+              surfaceTintColor: const Color.fromARGB(255, 255, 255, 255),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(40, 40, 40, 40),
+                child: Column(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: SizedBox(
+                        width: double.maxFinite,
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: GestureDetector(
+                            onTap: () {
+                              context.pop();
+                            },
+                            child: const MouseRegion(
+                              cursor: SystemMouseCursors.click,
+                              child: Icon(Icons.arrow_back),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 6,
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 40),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Sign up",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 28),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 20),
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  hintText: "Email",
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  hintText: "Username",
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: SizedBox(
+                                width: 120,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(3),
+                                    ),
+                                  ),
+                                  child: const Text("Sign up"),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
