cae8a693aa70bc8e46ab187392a3d58185d8dcc6
Khip01 <aakhifathallah24@gmail.com>
2024-02-01 22:33:52 +0700

    feat: added dummy comments

---
+++ lib/dummy_data/dummy_comments.dart
@@ -0,0 +1,55 @@
+class DummyComments {
+
+  List<Map> comments = [
+    {
+      "username": "destroyer",
+      "message": "zzzzzwooshshh \n zzzzzwooshshh zzzzzwooshshh \n\nzzzzzwooshshhzzzzzwooshshhzzzzzwooshshh zzzzzwooshshh",
+      "profile_pic": "https://api.dicebear.com/7.x/thumbs/svg?seed=destroyer",
+      "day":"1 min ago",
+      "like": "0"
+    },
+    {
+      "username": "Khip01",
+      "message": "Aku lagi sedih, tapi kalo ada kamu engga ehe",
+      "profile_pic": "https://api.dicebear.com/7.x/thumbs/svg?seed=khip01",
+      "day":"3 min ago",
+      "like": "999"
+    },
+    {
+      "username": "Anonymous User",
+      "message": "Helo",
+      "profile_pic": "https://api.dicebear.com/7.x/thumbs/svg?seed=Anonymous%20User",
+      "day":"20 min ago",
+      "like": "2"
+    },
+    {
+      "username": "Mark",
+      "message": "Halo bang",
+      "profile_pic": "https://api.dicebear.com/7.x/thumbs/svg?seed=Mark",
+      "day":"1 hour ago",
+      "like": "8"
+    },
+    {
+      "username": "Zkajwakjjh",
+      "message": "asjasjadjadhakjd",
+      "profile_pic": "https://api.dicebear.com/7.x/thumbs/svg?seed=Zkajwakjjh",
+      "day":"20 hour ago",
+      "like": "1"
+    },
+    {
+      "username": "Kiw kiw",
+      "message": "halo :D",
+      "profile_pic": "https://api.dicebear.com/7.x/thumbs/svg?seed=Kiw%20kiw",
+      "day":"1d",
+      "like": "8"
+    },
+    {
+      "username": "Yntkts",
+      "message": "Oy",
+      "profile_pic": "https://api.dicebear.com/7.x/thumbs/svg?seed=Yntkts",
+      "day":"3d",
+      "like": "10"
+    },
+  ];
+
+}
